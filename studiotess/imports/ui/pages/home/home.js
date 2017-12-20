ac5059b15c0b0f2fa9d4c44add8fb42805082508
srninkovic <sr.ninkovic@gmail.com>
2017-12-21 00:36:26 +0100

    homepage design

---
+++ studiotess/imports/ui/pages/home/home.js
@@ -0,0 +1,30 @@
+import './home.html';
+
+Template.App_home.rendered = function() {
+  $('#fullpage').fullpage({
+		//Scrolling
+		css3: false,
+		scrollingSpeed: 800,
+		autoScrolling: true,
+		fitToSection: true,
+		fitToSectionDelay: 2000,
+		scrollBar: false,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopHorizontal: true,
+		continuousVertical: true,
+		touchSensitivity: 50,
+		normalScrollElementTouchThreshold: 5,
+		bigSectionsDestination: null,
+		keyboardScrolling: true,
+		controlArrows: true,
+		verticalCentered: true,
+		responsiveWidth: 0,
+		responsiveHeight: 0,
+		responsiveSlides: false,
+		parallax: true,
+		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+		sectionSelector: '.section',
+		lazyLoading: true,
+	});
+}
