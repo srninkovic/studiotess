07f0179dc063e1a4723e47ea566734632e80899e
srninkovic <sr.ninkovic@gmail.com>
2017-12-21 01:16:14 +0100

    added pages

--- studiotess/imports/startup/client/routes.js
+++ studiotess/imports/startup/client/routes.js
@@ -5,12 +5,36 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
+import '../../ui/pages/reparaties/rep.js';
+import '../../ui/pages/contact/contact.js';
+import '../../ui/pages/collectie/collectie.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
+  },
+});
+
+FlowRouter.route('/reparaties', {
+  name: 'App.rep',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_rep' });
+  },
+});
+
+FlowRouter.route('/contact', {
+  name: 'App.contact',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_contact' });
+  },
+});
+
+FlowRouter.route('/collectie', {
+  name: 'App.coll',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_coll' });
   },
 });
 
